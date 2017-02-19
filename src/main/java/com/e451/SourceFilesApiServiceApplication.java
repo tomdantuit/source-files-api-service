e27f758f97bd09f3442c25b5277de1d30eac24dd
Tom Delaney <tom.delaney@8451.com>
2017-02-19 09:06:37 -0500

    initial commit

---
+++ src/main/java/com/e451/SourceFilesApiServiceApplication.java
@@ -0,0 +1,12 @@
+package com.e451;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceFilesApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceFilesApiServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/e451/SourceFilesApiServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.e451;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SourceFilesApiServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
