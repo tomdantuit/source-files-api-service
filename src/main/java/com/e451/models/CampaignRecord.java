a85681622634c7f430233d0ac5dcf40e83815f5b
Tom Delaney <tom.delaney@8451.com>
2017-02-23 16:23:32 -0500

    appeasing John...also now following better coding standards

--- src/main/java/com/e451/models/CampaignRecord.java
+++ src/main/java/com/e451/models/CampaignRecord.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 @Entity
 public class CampaignRecord {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private Long hshdKey;
     private String description;
